b25a71365f2ddb2eb9fb650bf9e9552baf1dd202
Allan Cheng <allanhfcheng@gmail.com>
2019-05-26 13:49:28 -0700

    added data.js

--- data.js
+++ data.js
@@ -1,5 +1,18 @@
 const STORE = [
     {
+        question: 'How did Daenerys Targaryen eventually hatch her dragon eggs?',
+        answers: [
+          'In a lightning storm',
+          'In a funeral pyre',
+          'In a fireplace.',
+          'In a frozen cave.'
+          ],
+          trivia:'At the end of Season 1, Daenerys Targaryen placed her three dragon eggs on the funeral pyre of her late husband. She then walked into the flames and emerged from the ashes the next morning holding three newly hatched dragons.',
+        correctAnswer: 'In a funeral pyre',
+        icon: 'HatchDragons.jpg',
+        alt: 'Daenerys holds egg'
+      },
+    {
       question: 'How did Daenerys Targaryen eventually hatch her dragon eggs?',
       answers: [
         'In a lightning storm',
@@ -10,6 +23,72 @@ const STORE = [
         trivia:'At the end of Season 1, Daenerys Targaryen placed her three dragon eggs on the funeral pyre of her late husband. She then walked into the flames and emerged from the ashes the next morning holding three newly hatched dragons.',
       correctAnswer: 'In a funeral pyre',
       icon: 'HatchDragons.jpg',
-      alt: 'tent icon'
-    }
+      alt: 'Daenerys holds egg'
+    },
+    {
+        question: 'Who plays Tyrion Lanister?',
+        answers: [
+          'Kit Harington',
+          'Nikolaj Coster-Waldau',
+          'Peter Dinklage',
+          'Iain Glen'
+          ],
+          trivia:'Peter Dinklage plays Tyrion Lanister.  He was not only the first person cast for the "Game of Thrones" series, but also the only person author George R.R. Martin wanted to play Tyrion.',
+        correctAnswer: 'Peter Dinklage',
+        icon: 'Tyrion.jpg',
+        alt: 'Tyrion'
+      },
+    {
+        question: 'What is the name of Jon Snow\'s sword',
+        answers: [
+          'Longclaw',
+          'Narsil',
+          'Master Sword',
+          'Glamdring'
+          ],
+          trivia:'Peter Dinklage plays Tyrion Lanister.  He was not only the first person cast for the "Game of Thrones" series, but also the only person author George R.R. Martin wanted to play Tyrion.',
+        correctAnswer: 'Peter Dinklage',
+        icon: 'LongClaw.jpg',
+        alt: 'Longclaw'
+      },
+    {
+        question: 'Who plays Tyrion Lanister?',
+        answers: [
+          'Kit Harington',
+          'Nikolaj Coster-Waldau',
+          'Peter Dinklage',
+          'Iain Glen'
+          ],
+          trivia:'Peter Dinklage plays Tyrion Lanister.  He was not only the first person cast for the "Game of Thrones" series, but also the only person author George R.R. Martin wanted to play Tyrion.',
+        correctAnswer: 'Peter Dinklage',
+        icon: 'Tyrion.jpg',
+        alt: 'Tyrion'
+      },
+    {
+        question: 'Who plays Tyrion Lanister?',
+        answers: [
+          'Kit Harington',
+          'Nikolaj Coster-Waldau',
+          'Peter Dinklage',
+          'Iain Glen'
+          ],
+          trivia:'Peter Dinklage plays Tyrion Lanister.  He was not only the first person cast for the "Game of Thrones" series, but also the only person author George R.R. Martin wanted to play Tyrion.',
+        correctAnswer: 'Peter Dinklage',
+        icon: 'Tyrion.jpg',
+        alt: 'Tyrion'
+      },
+
+      {
+        question: 'What is the name of Jon Snow\'s sword',
+        answers: [
+          'Longclaw',
+          'Narsil',
+          'Master Sword',
+          'Glamdring'
+          ],
+          trivia:'Peter Dinklage plays Tyrion Lanister.  He was not only the first person cast for the "Game of Thrones" series, but also the only person author George R.R. Martin wanted to play Tyrion.',
+        correctAnswer: 'Peter Dinklage',
+        icon: 'LongClaw.jpg',
+        alt: 'Longclaw'
+      }
 ];
