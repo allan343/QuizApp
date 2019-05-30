821957e488166d10743b4c0e912d006a6b6f6b6b
Allan Cheng <allanhfcheng@gmail.com>
2019-05-29 21:59:36 -0700

    red/green working

--- index.js
+++ index.js
@@ -46,22 +46,22 @@ function incrementScore () {
                     
                             <fieldset>
                                     <legend class="question">${STORE[questionNumber].question} </legend>
-                                    <div class =inputBorder>
-                                    <input type="radio" name="feelings-2" id="ans-great-1" value="${STORE[questionNumber].answers[0]}" checked>
+                                    <div class ="inputBorder answer1">
+                                    <input type="radio" name="feelings-2" id="ans-great-1" value="${STORE[questionNumber].answers[0]}">
                                     <label for="ans-great-1">${STORE[questionNumber].answers[0]}</label>
                                     </div>
                                     <br>
-                                    <div class =inputBorder>
+                                    <div class ="inputBorder answer2">
                                     <input type="radio" name="feelings-2" id="ans-great-2" value="${STORE[questionNumber].answers[1]}">
                                     <label for="ans-great-2">${STORE[questionNumber].answers[1]}</label>
                                     </div>
                                     <br>
-                                    <div class =inputBorder>
+                                    <div class ="inputBorder answer3">
                                     <input type="radio" name="feelings-2" id="ans-great-3" value="${STORE[questionNumber].answers[2]}">
                                     <label for="ans-great-3">${STORE[questionNumber].answers[2]}</label>
                                     </div>
                                     <br>
-                                    <div class =inputBorder>
+                                    <div class ="inputBorder answer4">
                                     <input type="radio" name="feelings-2" id="ans-great-4" value="${STORE[questionNumber].answers[3]}">
                                     <label for="ans-great-3">${STORE[questionNumber].answers[3]}</label>
                                     </div>
@@ -75,13 +75,53 @@ function incrementScore () {
                         </section>
     `;
   }
+
+function isAnswerCorrect(selection)
+{
+  
+  if($(selection).find('input').val() ===STORE[questionNumber].correctAnswer){
+    incrementScore();
+    $(selection).css("background-color", "green");
+    $(selection).css("color", "white");
+  }
+
+  else{
+    $(selection).css("background-color", "red");
+    $(selection).css("color", "white");
+      
+    if($(".answer1").find('input').val() ===STORE[questionNumber].correctAnswer){
+      $(".answer1").css("background-color", "green");
+      $(".answer1").css("color", "white");
+    }
+    if($(".answer2").find('input').val() ===STORE[questionNumber].correctAnswer){
+      $(".answer2").css("background-color", "green");
+      $(".answer2").css("color", "white");
+    }
+
+    if($(".answer3").find('input').val() ===STORE[questionNumber].correctAnswer){
+      $(".answer3").css("background-color", "green");
+      $(".answer3").css("color", "white");
+    }
+
+    if($(".answer4").find('input').val() ===STORE[questionNumber].correctAnswer){
+      $(".answer4").css("background-color", "green");
+      $(".answer4").css("color", "white");
+    }
+  
+  
+
+  }
+
+
+}
+
 function startQuiz()
 {
   console.log("lenght is "+STORE.length);
     $('.quizStart').on('click', '.startButton',function(event)
     {
-        $('.questionAnswerForm').css('display', 'block');
-        $('.quizStart').remove();
+      $('.questionAnswerForm').css('display', 'block');
+      $('.quizStart').remove();
        createQuestionImage();
        hideTrivia();
         
@@ -99,15 +139,41 @@ function renderQuestion(){
 }
 
 function userSelectAnswer(){
+  //changing background color
+  // set click handler on the div
+  // change child radio button to be checked
+  // just set background to be red
+  // set the correct id to be green
+  let selection = " ";
+  $('.questionAnswerForm').on('click', '.inputBorder',function(event)
+  {
+    $(this).find('input').prop('checked',true);
+
+    //console.log("this is " +this);
+    //  console.log("this is " + $(this).attr('class'));
+      selection= `.${$(this).attr('class').split(" ")[1]}`;
+      //selection = $(this).attr('class');
+    //  console.log("class is " + selection);
+      var radioValue = $(selection).find('input').val();
+      console.log("this value is " + radioValue);
+     // $(this).css("background-color", "green");
+     // $(this).css("color", "white");
+    //console.log("current target is " + event.currentTarget);
+    //this
+    //this event.currentTarget=
+    //background color
+  });
+
   $('.questionAnswerForm').on('submit',function(event)
   {
     event.preventDefault();
     var radioValue = $("input[name='feelings-2']:checked").val();
     console.log("correct answer is " + STORE[questionNumber].correctAnswer);
-    if(radioValue === STORE[questionNumber].correctAnswer)
+   /* if(radioValue === STORE[questionNumber].correctAnswer)
     {
       incrementScore();
-    }
+    }*/
+    isAnswerCorrect(selection);
     $('.userSubmit').remove();
     showTrivia();
   });
