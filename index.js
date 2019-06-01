e52fd760090445295746774e9b74518e3cf8a706
Allan Cheng <allanhfcheng@gmail.com>
2019-05-31 23:46:14 -0700

    removed commented out code

--- index.js
+++ index.js
@@ -131,8 +131,6 @@ function startQuiz()
     $('.quizStart').on('click', '.startButton',function(event)
     {
       $('.questionAnswerForm').css('display', 'block');
-     // $('.questionAnswerForm').css('margin-left','auto');
-     // $('.questionAnswerForm').css('margin-right','auto');
       $('.quizStart').remove();
        createQuestionImage();
        hideTrivia();
