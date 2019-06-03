b3acac6022f5625559163a0c6adae87327b97657
Allan Cheng <allanhfcheng@gmail.com>
2019-06-02 22:28:08 -0700

    fixed input label

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ function incrementScore () {
   function incrementQuestionNumber () {
     questionNumber++;
    setQuestionNumber();
-    console.log("increment is " + questionNumber);
   }
 
   function createQuestionImage() {
@@ -55,23 +54,23 @@ function incrementScore () {
                             <fieldset>
                                     <legend class="question">${STORE[questionNumber].question} </legend>
                                     <div class ="inputBorder answer1">
-                                    <input type="radio" name="feelings-2" id="ans-great-1" required value="${STORE[questionNumber].answers[0]}">
+                                    <input type="radio" name="answers" id="ans-great-1" required value="${STORE[questionNumber].answers[0]}">
                                     <label for="ans-great-1">${STORE[questionNumber].answers[0]}</label>
                                     </div>
                                     <br>
                                     <div class ="inputBorder answer2">
-                                    <input type="radio" name="feelings-2" id="ans-great-2" required value="${STORE[questionNumber].answers[1]}">
+                                    <input type="radio" name="answers" id="ans-great-2" required value="${STORE[questionNumber].answers[1]}">
                                     <label for="ans-great-2">${STORE[questionNumber].answers[1]}</label>
                                     </div>
                                     <br>
                                     <div class ="inputBorder answer3">
-                                    <input type="radio" name="feelings-2" id="ans-great-3" required value="${STORE[questionNumber].answers[2]}">
+                                    <input type="radio" name="answers" id="ans-great-3" required value="${STORE[questionNumber].answers[2]}">
                                     <label for="ans-great-3">${STORE[questionNumber].answers[2]}</label>
                                     </div>
                                     <br>
                                     <div class ="inputBorder answer4">
-                                    <input type="radio" name="feelings-2" id="ans-great-4" required value="${STORE[questionNumber].answers[3]}">
-                                    <label for="ans-great-3">${STORE[questionNumber].answers[3]}</label>
+                                    <input type="radio" name="answers" id="ans-great-4" required value="${STORE[questionNumber].answers[3]}">
+                                    <label for="ans-great-4">${STORE[questionNumber].answers[3]}</label>
                                     </div>
                                   </fieldset>
                                   <button class = "userSubmit" type="submit">Submit</button>
@@ -86,12 +85,10 @@ function incrementScore () {
     else{
      showResults();
       restartQuiz();
-     // $('.questionNumber').text(10)
     }
   }
 
-function isAnswerCorrect(selection)
-{
+function isAnswerCorrect(selection){
   
   if($(selection).find('input').val() ===STORE[questionNumber].correctAnswer){
     incrementScore();
@@ -125,61 +122,34 @@ function isAnswerCorrect(selection)
 
 }
 
-function startQuiz()
-{
-  console.log("lenght is "+STORE.length);
+function startQuiz(){
     $('.quizStart').on('click', '.startButton',function(event)
     {
       $('.questionAnswerForm').css('display', 'block');
       $('.quizStart').remove();
        createQuestionImage();
        hideTrivia();
-        
     });
-
 }
 
 function renderQuestion(){
 
-    console.log("render is "+questionNumber);
     $('.questionAnswerForm').html(createQuestion());
     hideSubmitButton();
-
 }
 
 function userSelectAnswer(){
 
   let selection = " ";
- /*
-  $('input:radio[name="feelings-2"]').change(
-    function(){
-        if (this.checked) {
-            // note that, as per comments, the 'changed'
-            // <input> will *always* be checked, as the change
-            // event only fires on checking an <input>, not
-            // on un-checking it.
-            // append goes here
-           
-            selection= `.${$(this).parent().attr('class').split(" ")[1]}`;
-            console.log("checked is " + selection);
-            showSubmitButton();
-        }
-    });
-*/
-  $('.questionAnswerForm').on('click', '.inputBorder',function(event)
-  {
+
+  $('.questionAnswerForm').on('click', '.inputBorder',function(event){
     $(this).find('input').prop('checked',true);
       selection= `.${$(this).attr('class').split(" ")[1]}`;
-      console.log("this value is " + selection);
       showSubmitButton();
-   
   });
 
-  $('.questionAnswerForm').on('submit',function(event)
-  {
+  $('.questionAnswerForm').on('submit',function(event){
     event.preventDefault();
-    var radioValue = $("input[name='feelings-2']:checked").val();
-    console.log("correct answer is " + STORE[questionNumber].correctAnswer);
     isAnswerCorrect(selection);
     $('.userSubmit').remove();
     showTrivia();
@@ -187,15 +157,10 @@ function userSelectAnswer(){
 }
 
 function renderNextQuestion(){
-    console.log("next render is "+questionNumber);
     $('.questionAnswerForm').on('click', '.nextQuestion',function(event)
     {
       event.preventDefault();
     incrementQuestionNumber();
-   // createQuestionImage();
-    
-    console.log("inside render is "+questionNumber);
-    
     $('.questionAnswerForm').html(createQuestion());
     createQuestionImage();
     hideSubmitButton();
